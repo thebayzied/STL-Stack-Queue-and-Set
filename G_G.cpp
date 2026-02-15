0c7fcba5e20241016122dd16310b7f9ae3a0f067
Bayzied Hassan <bayjed20@gmail.com>
2026-02-15 23:12:31 +0600

    Add files via upload

--- G_G.cpp
+++ G_G.cpp
@@ -5,16 +5,16 @@ int main (){
     int t; cin >> t;
     while(t--){
         int n; cin >> n;
-        priority_queue<int, vector <int>, greater <int>> pq; //minHEAP
-        for(int i = 0; i < n; i++){
-            int x; cin >> x;
+        priority_queue<ll, vector <ll>, greater <ll>> pq; //minHEAP
+        for(ll i = 0; i < n; i++){
+            ll x; cin >> x;
             pq.push(x);
         }
         ll sum = 0;
         while(pq.size() > 1){
-            int a = pq.top(); pq.pop();
-            int b = pq.top(); pq.pop();
-            int res = a + b;
+            ll a = pq.top(); pq.pop();
+            ll b = pq.top(); pq.pop();
+            ll res = a + b;
             sum += res;
             pq.push(res);
         }
